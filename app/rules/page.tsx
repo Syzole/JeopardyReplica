30813f1e9c5b8b1937b171a947a4e11147881a55
Syzole <108027056+Syzole@users.noreply.github.com>
2024-11-17 16:17:25 -0500

    Refactor team addition logic to streamline response handling and improve UI structure

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -55,88 +55,71 @@ const App: React.FC = () => {
         console.log(response);
 
         if (response.status !== 400) {
-            console.log(response);
-
-            if (response.status !== 400) {
-                setTeamName('');
-                loadTeams(); // Reload teams after adding
-                setHasAdded(true);
-                setHasAdded(true);
-                socket.emit('teams'); // Notify clients of updated
-                return;
-                return;
-            }
-
-            window.alert('Team name already exists');
-            setHasAdded(false);
             setTeamName('');
+            loadTeams(); // Reload teams after adding
+            setHasAdded(true);
+            socket.emit('teams'); // Notify clients of updated
+            return;
+        }
 
-            window.alert('Team name already exists');
-            setHasAdded(false);
-            setTeamName('');
-        };
-
-        return (
-            <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
-                <div className="text-center">
-                    <Rules showAdminNotes={ false } />
-                </div>
-                {/* Add team section */ }
-                <div className="mb-8 w-full max-w-lg flex flex-col items-center">
-                    <h2 className="text-2xl font-semibold mb-4">Create Teams:</h2>
-
-                    <div className="flex space-y-4 flex-col">
-                        <Input
-                            fullWidth
-                            placeholder="Team Name"
-                            value={ teamName }
-                            onChange={ (e) => setTeamName(e.target.value) }
-                            className={ "mr-4 " }
-                            className={ "mr-4 " }
-                        />
-                        <Button
-                            onClick={ addTeam }
-                            disabled={ hasAdded } // Keep the button disabled when `hasAdded` is true
-                            color={ hasAdded ? 'success' : 'primary' }
-                        >
-                            { hasAdded ? 'Team Added!' : 'Add Team' }
-                        </Button>
-                        <Button
-                            onClick={ addTeam }
-                            disabled={ hasAdded } // Keep the button disabled when `hasAdded` is true
-                            color={ hasAdded ? 'success' : 'primary' }
-                        >
-                            { hasAdded ? 'Team Added!' : 'Add Team' }
-                        </Button>
-                    </div>
-
-                    <div className="mt-8">
-                        { teams.length > 0 ? (
-                            <div>
-                                <h3 className="text-xl font-semibold mb-2">Teams:</h3>
-                                <ul className="space-y-2">
-                                    { teams.map((team, index) => (
-                                        <li key={ index } className="bg-gray-800 p-4 rounded-lg flex justify-between">
-                                            <div>
-                                                <span className="font-bold">{ team.name }</span>: { team.points } points
-                                            </div>
-                                        </li>
-                                    )) }
-                                </ul>
-                            </div>
-                        ) : (
-                            <p className="text-gray-400">No teams created yet.</p>
-                        ) }
-                    </div>
-                </div>
-                <div className="text-center mb-8">
-                    <p className="text-2xl font-semibold mb-4">Once you&apos;re done, click the button to head to the buzzer page</p>
-                    <Link href="/buzzerPage">
-                        <Button>Go to buzzer</Button>
-                    </Link>
-                </div>
-            </div>
-        );
+        window.alert('Team name already exists');
+        setHasAdded(false);
+        setTeamName('');
     };
 
-    export default App;
+    return (
+        <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+            <div className="text-center">
+                <Rules showAdminNotes={ false } />
+            </div>
+            {/* Add team section */ }
+            <div className="mb-8 w-full max-w-lg flex flex-col items-center">
+                <h2 className="text-2xl font-semibold mb-4">Create Teams:</h2>
+
+                <div className="flex space-y-4 flex-col">
+                    <Input
+                        fullWidth
+                        placeholder="Team Name"
+                        value={ teamName }
+                        onChange={ (e) => setTeamName(e.target.value) }
+                        className={ "mr-4 " }
+                    />
+                    <Button
+                        onClick={ addTeam }
+                        disabled={ hasAdded } // Keep the button disabled when `hasAdded` is true
+                        color={ hasAdded ? 'success' : 'primary' }
+                    >
+                        { hasAdded ? 'Team Added!' : 'Add Team' }
+                    </Button>
+                </div>
+
+                <div className="mt-8">
+                    { teams.length > 0 ? (
+                        <div>
+                            <h3 className="text-xl font-semibold mb-2">Teams:</h3>
+                            <ul className="space-y-2">
+                                { teams.map((team, index) => (
+                                    <li key={ index } className="bg-gray-800 p-4 rounded-lg flex justify-between">
+                                        <div>
+                                            <span className="font-bold">{ team.name }</span>: { team.points } points
+                                        </div>
+                                    </li>
+                                )) }
+                            </ul>
+                        </div>
+                    ) : (
+                        <p className="text-gray-400">No teams created yet.</p>
+                    ) }
+                </div>
+            </div>
+            <div className="text-center mb-8">
+                <p className="text-2xl font-semibold mb-4">Once you&apos;re done, click the button to head to the buzzer page</p>
+                <Link href="/buzzerPage">
+                    <Button>Go to buzzer</Button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default App;
